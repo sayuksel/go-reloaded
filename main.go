923d39b95f9fcba3d6afca77e8ad3acb14aac568
sayuksel <saloohyplayz2005@gmail.com>
2024-04-03 17:52:50 +0300

    put back all functions in main and added vowels (not complete)

--- main.go
+++ main.go
@@ -3,16 +3,143 @@ package main
 import (
 	"fmt"
 	"os"
-	"reloaded"
 	"strconv"
 )
 
-func First_letter(letter rune, i int, file string) bool {
-	if file[i+1] != ' ' && file[i-1] != ' ' {
-		return false
-	} else {
+func Capitalize(start, end int, file string) string { // this function takes a string and capitalizes the first letter and makes other lowercase if not the first letter
+	var result string
+	wordStart := true
+	for _, char := range file {
+		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9') {
+			if wordStart {
+				if char >= 'a' && char <= 'z' {
+					result += string(char - 32)
+				} else {
+					result += string(char)
+				}
+				wordStart = false
+			} else {
+				if char >= 'A' && char <= 'Z' {
+					result += string(char + 32)
+				} else {
+					result += string(char)
+				}
+			}
+		} else {
+			if char >= 'A' && char <= 'Z' {
+				result += string(char + 32)
+			} else if char >= '0' && char <= '9' {
+				result += string(char)
+			} else {
+				wordStart = true
+				result += string(char)
+			}
+		}
+	}
+	return result
+}
+
+func to_upper(start, end int, file []byte) []byte { // this function receives the start and end point of the words and capitalizes them
+	i := start
+	for _, v := range file[start:end] {
+		if v >= 'a' && v <= 'z' {
+			file[i] = v - 32
+		}
+		i++
+	}
+	return file
+}
+
+func to_lower(start, end int, file []byte) []byte { // this function receives the start and end point of the words and makes them lower case
+	i := start
+	for _, v := range file[start:end] {
+		if v >= 'A' && v <= 'Z' {
+			file[i] = v + 32
+		}
+		i++
+	}
+	return file
+}
+
+func transfer(sample_file string, result_file *os.File) *os.File { // this function will transfer the data from sample.txt to result.txt which will be outputted
+	flag := false
+	for _, v := range sample_file {
+		if v == '(' {
+			flag = true
+		}
+		if !flag {
+			_, err := result_file.WriteString(string(v))
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			if v == ')' {
+				flag = false
+			}
+		}
+	}
+	return result_file
+}
+
+func if_words(end int, file string) (int, int) { // this function will make sure if there is more than one word to be manipulated
+	flag := false
+	words := 1
+	for _, v := range file[end:] {
+		if !flag {
+			if v == ')' {
+				flag = true
+				break
+			} else {
+				if v >= '1' && v <= '9' {
+					words = int(v) - 48
+				}
+			}
+		}
+	}
+	if words > 1 {
+		r_end, start := prev_words(end, words, file)
+		return r_end, start
+	}
+	r_end, start := prev_word(end, file)
+	return r_end, start
+}
+
+func prev_words(end, words int, file string) (int, int) { // this function will give u the start and end valus of the word to be manipulated if there is more than one word
+	flag := 0
+	start := end - 3
+	end -= 2
+	for flag < words {
+		if (string(file[start]) != " ") && (start > 0) {
+			start--
+		} else {
+			flag++
+			start--
+		}
+	}
+	start++
+	return end, start
+}
+
+func prev_word(end int, file string) (int, int) { // this function will give u the start and end valus of the word to be manipulated if there is only one word
+	flag := false
+	start := end - 3
+	end -= 2
+	for !flag {
+		if string(file[start]) != " " {
+			start--
+		} else {
+			flag = true
+		}
+	}
+	start++
+	return end, start
+}
+
+func if_vowel(i int, file string) bool {
+	if file[i] == 'a' || file[i] == 'e' || file[i] == 'i' || file[i] == 'o' || file[i] == 'u' || file[i] == 'h' {
 		return true
 	}
+	return false
 }
 
 func main() {
@@ -29,36 +156,31 @@ func main() {
 	// 	fmt.Println(err)
 	// }
 	// result_data, err := os.WriteFile(input[0])
-
 	// result_data, err := os.Open(input[1])
 	// if err != nil {
 	// 	fmt.Println(err)
 	// }
-
 	sample_data, err := os.ReadFile(input[0])
 	if err != nil {
 		fmt.Println(err)
 	} else {
 		for i, v := range sample_data {
-			if string(v) == "(" { // for hex, bin, up, low and cap
+			if string(v) == "(" {
 				i++
 				switch string(sample_data[i]) {
 				case "u":
-					end, start = reloaded.if_words(i, string(sample_data))
-					sample_data = reloaded.to_upper(start, end, sample_data)
-
+					end, start = if_words(i, string(sample_data))
+					sample_data = to_upper(start, end, sample_data)
 				case "l":
-					end, start = reloaded.if_words(i, string(sample_data))
-					sample_data = reloaded.to_lower(start, end, sample_data)
+					end, start = if_words(i, string(sample_data))
+					sample_data = to_lower(start, end, sample_data)
 					fmt.Println(string(sample_data))
-
 				case "c":
-					end, start = reloaded.if_words(i, string(sample_data))
-					sample_data = []byte(reloaded.Capitalize(start, end, string(sample_data)))
+					end, start = if_words(i, string(sample_data))
+					sample_data = []byte(Capitalize(start, end, string(sample_data)))
 					fmt.Println(string(sample_data))
-
 				case "h":
-					end, start = reloaded.if_words(prev_word, i, string(sample_data))
+					end, start = prev_word(i, string(sample_data))
 					h_decimal := sample_data[start:end]
 					decimal, err := strconv.ParseInt(string(h_decimal), 16, 64)
 					if err != nil {
@@ -66,9 +188,8 @@ func main() {
 					} else {
 						fmt.Println("Decimal :", decimal)
 					}
-
 				case "b":
-					end, start = reloaded.prev_word(i, string(sample_data))
+					end, start = prev_word(i, string(sample_data))
 					b_decimal := sample_data[start:end]
 					decimal, err := strconv.ParseInt(string(b_decimal), 2, 64)
 					if err != nil {
@@ -78,18 +199,32 @@ func main() {
 					}
 				}
 			}
-			// if v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u' {
-			// first := First_letter(rune(v), i, string(sample_data))
-			// }
+
+			if v == 'a' || v == 'A' { // check for vowels
+				if i > 0 { // checks if not first letter
+					if sample_data[i-1] == ' ' && sample_data[i+1] == ' ' { // it makes sure that the 'a' is alone with no letter before or after it
+						vowel := if_vowel(i+2, string(sample_data))
+						if vowel {
+							// make a to an
+						}
+					}
+				} else {
+					if sample_data[i+1] == ' ' {
+						vowel := if_vowel(i+2, string(sample_data))
+						if vowel {
+							// make a to an
+						}
+					}
+				}
+			}
 		}
-		result_data := reloaded.transfer(string(sample_data))
-		fmt.Println(result_data)
+		// result_data := transfer(string(sample_data), result_data)
+		fmt.Println(string(sample_data))
 	}
 }
 
 // end, start := prev_word(i, string(data))
 // fmt.Println(string(data[start:end]))
-
 // decimal, err := strconv.ParseInt(hexStr, 16, 64)
 //     if err != nil {
 //         fmt.Println("Error:", err)
